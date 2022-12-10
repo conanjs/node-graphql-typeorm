cd2ede7b3af2f64b935464fedb770e373b9a797e
conanjs <doquangvinh0708co@gmail.com>
2022-12-10 12:02:16 +0700

    update path to envConfig
    update path to envConfig

--- src/@Database/PostgreSQL/migration/migration.ts
+++ src/@Database/PostgreSQL/migration/migration.ts
@@ -1,6 +1,6 @@
-import { isProduction } from "src/@Config";
 import { logger } from "../../../@Commons/loggers/LoggerService";
 import { AppDataSource } from "./app-data-source";
+import { isProduction } from '../../../@Config/env';
 
 const startPostgreSQLDatabaseMigration = async () => {
     return AppDataSource.initialize()
